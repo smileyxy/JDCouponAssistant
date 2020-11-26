2805d5bdf5ef12bd7fd1e77ce19a7d7d439c32b6
smileyxy <smalllk@126.com>
2020-11-26 21:23:06 +0800

    update pet

--- game/jdjoy.ts
+++ game/jdjoy.ts
@@ -422,7 +422,7 @@ export default class JdJoy implements Game {
                     Utils.outPutLog(this.outputTextarea, `${currentJDDate.toLocaleString()} 已开启自动换豆！`, false);
 
                     let firstSpan = defaultBeanDetection - currentJDDate.getMinutes() * 60000 + Utils.random(0, 3000);
-                    this.newExchange();
+
                     autoBeanTimeout = setTimeout(() => {
                         this.newExchange();
                         beanInterval = setInterval(() => {
